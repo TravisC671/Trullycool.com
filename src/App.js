fba03c03ffeac8d445144ce2ca2706fa470dd3cd
TravisC671 <63769252+TravisC671@users.noreply.github.com>
2020-11-30 19:50:59 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import { ReactComponent as Logo } from "./assets/Logo.svg";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>welcome to Trullycool.com</h1>
+      <h5>more coming soon</h5>
+      <Logo />
+      <h5>by Travis Calhoun(I own this)</h5>
+    </div>
+  );
+}
