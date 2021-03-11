9e8bc82f8a557ee3826c3212747563c61c59bea5
TravisC671 <63769252+TravisC671@users.noreply.github.com>
2021-03-11 11:14:46 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
       <h1>welcome to Trullycool.com</h1>
       <h5>more coming soon</h5>
       <Logo />
+      <h5>the island would not have been different</h5>
       <h5>by Travis Calhoun(I own this)</h5>
       <button>Test</button>
     </div>
